f2e9d358df79e4e5c650bddaf1a72627de048d80
DESKTOP-PO1DF5V\sonic <mnemosineii@gmail.com>
2018-01-27 01:14:42 +0100

    GameManager

---
+++ GGJ2018/Assets/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    private static GameManager instance = null;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            return GameManager.instance;
+        }
+    }
+
+    void Awake()
+    {
+        if (GameManager.instance == null)
+            GameManager.instance = this;
+        else if (GameManager.instance != this)
+            GameObject.Destroy(this.gameObject);
+        // No destruir con los cambios de escena
+        GameObject.DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void LoadeScene(string scn)
+    {
+        if (scn == "reset")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        else
+        {
+            SceneManager.LoadScene(scn);
+        }
+
+    }
+}
