e6a44fd14d5272b3987f607de14506f598170088
Albersan\Alberto Sánchez Sánc <kerorito@gmail.com>
2018-01-27 15:52:35 +0100

    Añado boquete y caida

--- GGJ2018/Assets/Scripts/Player/PlayerController.cs
+++ GGJ2018/Assets/Scripts/Player/PlayerController.cs
@@ -3,14 +3,56 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+    public float Velocidad = 2.0F;
+    Rigidbody2D rb;
+    bool caida = false;
 
-	// Use this for initialization
-	void Start () {
-		
+    void Start () {
+        rb = GetComponent<Rigidbody2D> ();
+
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	void FixedUpdate () {
+        if (!caida)
+        {
+            Movimiento();
+        }
+        else {
+            Caida();
+        }
+    }
+    void OnTriggerEnter2D(Collider2D coll) {
+        if (coll.gameObject.tag.Equals("Boquete"))
+        {
+            caida = true;
+            Debug.Log("caida");
+        }
+
+    }
+    
+
+        void Movimiento() {
+            float EjeX =  Input.GetAxis("Horizontal");
+            float EjeY =  Input.GetAxis("Vertical");
+        Vector2 mov = new Vector2(EjeX*Velocidad ,EjeY*Velocidad) ;
+        rb.velocity = mov;
+    }
+    void Caida() {
+        Debug.Log("cayendo");
+        transform.localScale = new Vector3(Mathf.Lerp(transform.localScale.x, 0f, Time.fixedDeltaTime),
+        Mathf.Lerp(transform.localScale.y, 0f, Time.fixedDeltaTime), 1.0f);
+
+        //transform.localScale = new Vector3(1.0f, 1.0f, 20.0f);
+
+        if (transform.localScale.x <= 6f)
+        {
+            caida = false;
+            transform.position = new Vector3(0, 0, -10);
+            transform.localScale = new Vector2(10, 10);
+        }
+    }
+
+    
+
+
 }
